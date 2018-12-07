3eb69b76cfe8b87d2f1a95811b9238621859784d
System Administrator <root@Jons-MacBook.local>
2018-12-07 21:54:54 +0000

    introducing curved sliders

--- src/hiveFrame.py
+++ src/hiveFrame.py
@@ -78,6 +78,9 @@ class windowFrame(QtWidgets.QGraphicsView):
             item.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable)
             self.scene().addItem(item)
             self.writeText("Refresh", circX, circY, w, h)
+            
+            sslider = pathSlider(minimum=0, maximum=100)
+            self.scene().addWidget(sslider)
         
         except ZeroDivisionError:
             sys.exit("Unable to connect to the server")
